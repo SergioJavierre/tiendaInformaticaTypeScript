3cab4ab398807662b7fc11b11dfccf7f1bba526b
sergiojavierre <sergiojavierre91@gmail.com>
2020-10-27 17:12:29 +0100

    Initial commit

---
+++ funciones/articulos.ts
@@ -0,0 +1,37 @@
+import { read } from "fs"
+
+const readline = require('readline')
+const fs = require('fs')
+
+const pathArticulos = './datos/articulos.txt'
+
+const leerArticulos = () => {
+    const lector = readline.createInterface({
+        input:fs.createReadStream(pathArticulos),
+        output:process.stdin,
+        terminal:false
+    })
+    lector.on('line',leeLineaArticulo)
+}
+
+const insertaArticulo = (articulo) => {
+    const datos = "\n"+articulo.nombre+";"+articulo.precio //\n para meter nueva línea
+    //añadimos al final del fichero
+    fs.appendFile('./datos/articulos.txt',datos, (err) => {
+        if(err){
+            console.log('Se ha producido un error en la escritura', err)
+        }
+        console.log('Cambios guardados')
+    })
+}
+
+const leeLineaArticulo = (linea) =>{
+    let datosArticulo = linea.split(";")
+    let articulo = {
+        nombre: datosArticulo[0],
+        precio: datosArticulo[1]
+    }
+    console.log(articulo)
+}
+
+export {leerArticulos, insertaArticulo}
---
+++ main.ts
@@ -0,0 +1,6 @@
+
+import { leerArticulos, insertaArticulo } from './funciones/articulos'
+
+//insertaArticulo({nombre: "RX 580", precio: 150})
+
+leerArticulos()
